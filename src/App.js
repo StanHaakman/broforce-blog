1624c904a25d43ec1a33f2efcce3ebd89f7398ea
stanhaakman <stanhaakman@icloud.com>
2022-03-07 20:51:02 +0100

    Lesson 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.sass';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="content">
-          <h1 className="text-4xl font-bold text-center">App Component</h1>
-      </div>
-    </div>
-  );
+    const title = 'Welcome to the new blog';
+    const likes = 50;
+    const person = {name: 'yoshi', age: 30};
+    const googleLink = "https://www.google.com";
+
+      return (
+        <div className="App">
+          <div className="content text-center">
+              <h1 className="text-4xl font-bold">{title}</h1>
+              <p>Liked {likes} times</p>
+
+              <p>{10}</p>
+              <p>{"hello ninjas"}</p>
+              <p>{[1,2,3,4,5,6]}</p>
+              <p>{Math.random() * 10}</p>
+
+              <a href={googleLink} target={"_blank"}>Google site</a>
+          </div>
+        </div>
+      );
 }
 
 export default App;
