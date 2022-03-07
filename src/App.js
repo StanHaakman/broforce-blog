4dff1d7c6a404b5f62a256eb0873de83ce28f069
stanhaakman <stanhaakman@icloud.com>
2022-03-07 21:22:13 +0100

    Lesson 5 finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import './App.sass';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
-    const title = 'Welcome to the new blog';
-    const likes = 50;
-    const person = {name: 'yoshi', age: 30};
-    const googleLink = "https://www.google.com";
-
       return (
         <div className="App">
+            <Navbar />
           <div className="content text-center">
-              <h1 className="text-4xl font-bold">{title}</h1>
-              <p>Liked {likes} times</p>
-
-              <p>{10}</p>
-              <p>{"hello ninjas"}</p>
-              <p>{[1,2,3,4,5,6]}</p>
-              <p>{Math.random() * 10}</p>
-
-              <a href={googleLink} target={"_blank"}>Google site</a>
+              <Home />
           </div>
         </div>
       );
