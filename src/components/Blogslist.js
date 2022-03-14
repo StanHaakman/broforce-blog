5aca80f33e4e508160b9adcc10cacbfbbd792f96
stanhaakman <stanhaakman@icloud.com>
2022-03-14 19:46:00 +0100

    Lesson 11 helemaal afgerond

--- src/components/Blogslist.js
+++ src/components/Blogslist.js
@@ -4,7 +4,7 @@ const Bloglist = (props) => {
 
     return (
         <div className="blog-list">
-            <h2 className="text-center">{title}</h2>
+            <h2 className="text-center text-lg">{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview p-10 my-4 bg-gray-100 rounded-xl hover:bg-gray-200 hover:cursor-pointer hover:shadow-lg hover:rounded-3xl transition-all" key={blog.id}>
                     <h3 className="text-xl font-medium">{blog.title}</h3>
