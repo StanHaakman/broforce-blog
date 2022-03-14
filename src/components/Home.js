1f0e88d452f3ab318dca4267f1590d1297599df7
stanhaakman <stanhaakman@icloud.com>
2022-03-14 21:13:53 +0100

    Lesson 16 afgerond

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,8 +9,6 @@ const Home = () => {
         {title: 'Web dev top tips', body: 'lorem ipsoum...', author: 'Luigi', id: 3}
     ]);
 
-    const [name, setName] = useState('Mario');
-
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs)
@@ -20,15 +18,13 @@ const Home = () => {
         console.log("use effect ran")
         return () => {
         };
-    }, [name]);
+    }, []);
 
 
     return (
       <div className="home">
-          <button onClick={() => setName((name === 'Luigi' ? 'Mario': 'Luigi'))}>Change name</button>
 
           <Bloglist blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
-          <Bloglist blogs={blogs.filter((blog) => blog.author === name)} title={name + " blogs"} handleDelete={handleDelete}/>
       </div>
     );
 }
